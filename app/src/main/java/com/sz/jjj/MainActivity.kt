5e5ffb963f4688ed3ba5df993eb41012a5e48c72
JJJ <884604421@qq.com>
2017-06-27 11:00:42 +0800

    Initial commit

---
+++ app/src/main/java/com/sz/jjj/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sz.jjj
+
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recyclerView.setBackgroundColor(Color.BLACK)
+    }
+}
