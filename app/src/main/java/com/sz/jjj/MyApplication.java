4627a0c8fbf64a6d38f4f03656fdcbdd7177228b
JJJ <884604421@qq.com>
2017-07-28 17:59:57 +0800

    添加webview加载进度条、vidioview

---
+++ app/src/main/java/com/sz/jjj/MyApplication.java
@@ -0,0 +1,38 @@
+package com.sz.jjj;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.view.WindowManager;
+
+/**
+ * Created by jjj on 2017/7/28.
+ *
+ * @description:
+ */
+
+public class MyApplication extends Application {
+
+    public final static float DESIGN_WIDTH = 750; //绘制页面时参照的设计图宽度
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        resetDensity();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        resetDensity();
+    }
+
+    public void resetDensity() {
+        Point size = new Point();
+        ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay().getSize(size);
+
+        getResources().getDisplayMetrics().xdpi = size.x / DESIGN_WIDTH * 72f;
+    }
+
+}
---
+++ app/src/main/java/com/sz/jjj/activity/VideoViewActivity.kt
@@ -0,0 +1,25 @@
+package com.sz.jjj.activity
+
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.sz.jjj.R
+import kotlinx.android.synthetic.main.activity_videoview.*
+
+/**
+ * Created by jjj on 2017/7/26.
+@description:
+ */
+class VideoViewActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_videoview)
+
+        val uri = Uri.parse("android.resource://cn.bluemobi.dylan.welcomevideopager/raw/" + R.raw.test1)
+        play.setOnClickListener(View.OnClickListener { videoView.playVideo(uri) })
+        pause.setOnClickListener(View.OnClickListener { videoView.pause() })
+        start.setOnClickListener(View.OnClickListener { videoView.start() })
+        stop.setOnClickListener(View.OnClickListener { videoView.stopPlayback() })
+    }
+}
