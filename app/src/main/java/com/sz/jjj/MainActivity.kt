08026fdaf742a17b11b227990f9de41d30839ab2
JJJ <884604421@qq.com>
2017-08-23 15:12:39 +0800

    xml解析

--- app/src/main/java/com/sz/jjj/MainActivity.kt
+++ app/src/main/java/com/sz/jjj/MainActivity.kt
@@ -17,12 +17,14 @@ class MainActivity : AppCompatActivity() {
             MeituanBottomNavActivity::class.java, EmptyRecyclerviewActivity::class.java,
             VideoViewActivity::class.java, WebViewActivity::class.java,
             ListernNotificationActivity::class.java, DayNightModeActivity::class.java,
-            DataBaseActivity::class.java, Rotate3dAnimActivity::class.java)
+            DataBaseActivity::class.java, Rotate3dAnimActivity::class.java,
+            XmlParseActivity::class.java)
     private val TITLE = arrayOf(
             "仿美图底部导航", "Recyclerview空布局",
             "VideoView小试牛刀", "WebView进度条",
             "监听通知消息", "白天/夜晚切换",
-            "数据库操作", "3d翻转动画")
+            "数据库操作", "3d翻转动画",
+            "xml解析")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
